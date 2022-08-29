396a027f7bd5587c1ff8c5acacbc8e1241cccbf4
KrsPatrick <lesiewicz.patrick@gmail.com>
2022-08-29 13:32:47 +0200

    added searchbar2

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import MyComponent from "./src/components/searchbar.components";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  console.log(StatusBar.currentHeight);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.containter}>
+        <View style={styles.search}>
+          <MyComponent> searchbar</MyComponent>
+        </View>
+        <View style={styles.list}>
+          <Text>dos</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  containter: { flex: 1, marginTop: StatusBar.currentHeight },
+  search: { backgroundColor: "green", padding: 16 },
+  list: { flex: 1, padding: 16, backgroundColor: "blue" },
 });
